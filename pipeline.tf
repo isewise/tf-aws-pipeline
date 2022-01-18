be217a30c9a3d43c1a223e55a5ef9f0968ecefd2
Isaac Booker <iebooker@gmail.com>
2022-01-18 14:55:26 -0800

    terminate instance

--- pipeline.tf
+++ pipeline.tf
@@ -87,17 +87,6 @@ resource "aws_codepipeline" "tf-pipeline" {
       }
     }
   }
-
-  # stage {
-  #   name = "Approval"
-  #   action {
-  #     name = "Approval"
-  #     category = "Approval"
-  #     owner = "AWS"
-  #     provider = "CodeBuild"
-  #   }
-  # }
-
   stage {
     name = "Deploy"
     action {
