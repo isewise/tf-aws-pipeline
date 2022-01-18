9866239532ab50d77ff416cc4afe548978e665b3
Isaac Booker <iebooker@gmail.com>
2022-01-17 19:05:56 -0800

    instance terminated

--- GIT RESOURCES.tf
+++ GIT RESOURCES.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "MyAWSResource" {
-   instance_type   = "t2.micro"
-   ami = "ami-005e54dee72cc1d00"
-   tags = {
-     Name = "From GIT CI/CD"
-   }
-}
+# resource "aws_instance" "MyAWSResource" {
+#    instance_type   = "t2.micro"
+#    ami = "ami-005e54dee72cc1d00"
+#    tags = {
+#      Name = "From GIT CI/CD"
+#    }
+# }
